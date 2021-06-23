bdcd340e2798cc9e580a7f66acee71c65a8530ca
Abdulhamit Akaslan <hamtaksln@gmail.com>
2021-06-23 20:33:12 +0300

    home_page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import Head from "next/head"
 
-function HomePage(){
-
-     return <div className="bg-blue-200">Home</div>
+function HomePage() {
+    return (
+        <div className="site-container">
+            <Head>
+                <title>My project</title>
+                <meta
+                    name="viewport"
+                    content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1"
+                />
+            </Head>
+            <main>NextJS Tailwind CSS Boilerplate</main>
+        </div>
+    )
 }
 
 export default HomePage
